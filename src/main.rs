5e3efda1117110f580545e01d8a06c5afafbe4f2
Dinu Blanovschi <git@dnbln.dev>
2025-04-24 20:42:40 +0200

    aaaa

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,55 @@ use std::io::BufRead;
 use std::process::{Command, Stdio};
 
 fn main() {
-    let mut cmd = Command::new("cargo");
+    let first_arg = std::env::args().nth(1).unwrap_or_else(|| "test".to_string());
+    
+    let mut cmd = match first_arg.as_str() {
+        "test" => {
+            let mut cmd = Command::new("cargo");
 
-    let f = std::fs::File::create("nextest.stderr.log").unwrap();
+            let f = std::fs::File::create("test.stderr.log").unwrap();
 
-    cmd.args([
-        "nextest",
-        "run",
-        "--message-format",
-        "libtest-json-plus",
-        "--workspace",
-        "--no-fail-fast",
-        "--hide-progress-bar",
-        "--final-status-level",
-        "none",
-        "--status-level",
-        "none",
-        "--success-output",
-        "never",
-        "--failure-output",
-        "never",
-        "--no-input-handler",
-    ])
-    .env("NEXTEST_EXPERIMENTAL_LIBTEST_JSON", "1")
-    .stdin(Stdio::null())
-    .stdout(Stdio::piped())
-    .stderr(Stdio::from(f));
+            cmd.args([
+                "test",
+                "--workspace",
+                "--no-fail-fast",
+            ])
+                .stdin(Stdio::null())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::from(f));
+            cmd
+        }
+        "nextest" => {
+            let mut cmd = Command::new("cargo");
+
+            let f = std::fs::File::create("test.stderr.log").unwrap();
+
+            cmd.args([
+                "nextest",
+                "run",
+                "--message-format",
+                "libtest-json-plus",
+                "--workspace",
+                "--no-fail-fast",
+                "--hide-progress-bar",
+                "--final-status-level",
+                "none",
+                "--status-level",
+                "none",
+                "--success-output",
+                "never",
+                "--failure-output",
+                "never",
+                "--no-input-handler",
+            ])
+                .env("NEXTEST_EXPERIMENTAL_LIBTEST_JSON", "1")
+                .stdin(Stdio::null())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::from(f));
+            cmd
+        } 
+        _ => panic!("Unknown command: {}", first_arg),
+    };
 
     let mut child = cmd.spawn().unwrap();
     let stdout = child.stdout.take().unwrap();
